c02b98e8a1949a3e617270a0a060932e498c58d6
wharsojo <wharsojo@gmail.com>
2017-08-04 05:27:04 +0800

    add toggle buttons

--- graticule/src/Earth.js
+++ graticule/src/Earth.js
@@ -1,4 +1,3 @@
-// http://eng.wealthfront.com/2017/02/14/integrating-d3-into-react/
 import React, { Component } from 'react'
 import earthjs  from 'earthjs/core';
 import world from 'earthjs/example/d/world-110m.json';
@@ -9,7 +8,8 @@ import 'earthjs/example/css/earthjs.css'
 class Earth extends Component {
     constructor(props){
         super(props)
-        this.createEarth = this.createEarth.bind(this)
+        this.createEarth = this.createEarth.bind(this);
+        this.btnClick    = this.btnClick.bind(this);
     }
     componentDidMount() {
         this.createEarth()
@@ -17,27 +17,45 @@ class Earth extends Component {
     shouldComponentUpdate() {
         return false; // This prevents future re-renders of this component
     }
-    // componentDidUpdate() {
-    //     this.createEarth()
-    // }
     createEarth() {
         const {
             mousePlugin,
+            configPlugin,
             autorotatePlugin,
         } = earthjs.plugins;
         const g = earthjs()
         .register(mousePlugin())
+        .register(configPlugin())
         .register(canvasPlugin())
         .register(autorotatePlugin())
         .register(graticuleCanvas())
         .register(worldCanvas());
         g._.options.transparent = true;
-        g.worldCanvas.data(world);
+        g.worldCanvas.data(world); // get embedded topojson
         g.create();
+        this.g = g;
+    }
+    btnClick(e) {
+        const t = e.target.innerText;
+        const o = this.g._.options;
+        const c = this.g.configPlugin;
+        if (t==='transparent') {
+            c.set({transparent: !o.transparent});
+        } else if (t==='graticule') {
+            c.set({showGraticule: !o.showGraticule});
+        } else if (t==='land') {
+            c.set({showLand: !o.showLand});
+        }
     }
     render() {
-        return <svg id="earth-js">
-        </svg>
+        return (<div>
+            <svg id="earth-js"></svg>
+            <span className="ejs-buttons">
+                <button onClick={this.btnClick}>transparent</button>,
+                <button onClick={this.btnClick}>graticule</button>,
+                <button onClick={this.btnClick}>land</button>
+            </span>
+        </div>)
     }
 }
 export default Earth
